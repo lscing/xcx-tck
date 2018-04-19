bca38f6ce33419b2068b6528287d15e6b56d0018
lsc <1378674848@qq.com>
2018-04-19 12:01:39 +0800

    419

---
+++ timing/index.js
@@ -0,0 +1,104 @@
+// pages/sign/timing/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    lis:"<周五>",
+    leaveData:[
+      { id: "aaa",shqw:0,isFold:"true",content:"每周四,周五,周六的09:54发布签到,签到时间:09:55-09:56asdsadsadds到,签到时间:09:55-09:56asdsadsadds"},
+      { id: "bbb",shqw:0,isFold: "true",content: "每周四,周五,周六的09:54发布签到,签到时间:09:55-09:56" },
+      { id: "ccc", shqw:0,isFold: "true",content: "每周四,周五,周六的09:54发布签到,签到时间:09:55-09:56asdsadsadds到,签到时间:09:55-09:56asdsadsadds" },
+    ],
+    isOne: false,
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  
+  onLoad: function (options) {
+    
+  },
+  /**
+ * 生命周期函数--监听页面初次渲染完成
+ */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+   
+  },
+  flodFn: function (e) {
+
+
+
+
+    var data = this.data.leaveData;
+    var fid = e.currentTarget.dataset.id;
+    //这里实现了jq点击事件所谓的this
+    //通过遍历数组取得与当前id所相同的那一列数组
+    //然后给那一列数组的某个属性赋值
+    for (var i = 0; i < data.length; i++) {
+      if (fid == data[i].id){
+        //console.log("i",i)
+        data[i].isFold = !data[i].isFold;
+      }
+    }
+
+    this.setData({
+      leaveData: data
+    });
+  },
+  switch1Change: function (e) {
+    console.log('switch1 发生 change 事件，携带值为', e.detail.value)
+  },
+
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  },
+  addTiming: function () {
+      wx.navigateTo({
+        url: "/pages/sign/createTiming/index"
+      })
+    
+  },
+})
